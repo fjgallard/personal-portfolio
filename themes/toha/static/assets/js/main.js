a520353f049e5aeb470d121b258a5fbffd2abd3d
fjgallard <fjdgallardo@gmail.com>
2020-09-03 19:58:47 +0800

    Fixed scroll bug.

--- themes/toha/static/assets/js/main.js
+++ themes/toha/static/assets/js/main.js
@@ -35,11 +35,11 @@ var isMobile = false, isTablet = false, isLaptop = false;
           var hash = this.hash;
 
           let offset = 60;
-          if (isMobile) {
+          /* if (isMobile) {
             offset = 760;
           } else if (isTablet) {
             offset = 60;
-          }
+          } */
           // Using jQuery's animate() method to add smooth page scroll
           // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
           $('html, body').animate({
